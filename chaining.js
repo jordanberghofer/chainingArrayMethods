7c8a3e9832526b091e14e29dc4c345f95376c3bf
jordanberghofer <jordan.berghofer@gmail.com>
2017-08-09 15:59:44 -0500

    still working it out

--- chaining.js
+++ chaining.js
@@ -1,8 +1,23 @@
 console.log("chaining.js");
-
+let intAfterMath = 0
 let integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 integers.sort();
-integers.sort(function(first, second) {return first - second; }).reverse();
+integers.sort(function(first, second) {return first - second; }).reverse().splice(0, 4,);
 console.log("integers:", integers);
+
+return integers;
+
+function mathStuff(num) {
+    intAfterMath = integers(a, b)
+}
+
+
+// function filterNineteen() {
+//     integers.replace(0, 4, "");
+// }
+
+// console.log(filterNineteen());
+
+
 
